134fdd4b209564a5aff6563ec53bb777762f138d
Austin Condiff <austin.condiff@gmail.com>
2018-08-22 09:45:26 -0500

    Corrected typo in global headers

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const router = express.Router()
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DEconstE')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
   res.header('Access-Control-Expose-Headers', 'Content-Length')
   res.header(
     'Access-Control-Allow-Headers',
